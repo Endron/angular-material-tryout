7de37ad4e21910775bcb57c53abc9f5f3db6f39b
Tobias Kremer <tobias.kremer@comsysto.com>
2015-03-24 11:06:26 +0100

    disable caching of resources

--- app.groovy
+++ app.groovy
@@ -1,2 +1,12 @@
-@Controller class MainController { }
+@Controller
+class MainController { }
 
+@Configuration
+class MvcConfig extends WebMvcConfigurerAdapter {
+  @Override
+  void addResourceHandlers(final ResourceHandlerRegistry registry) {
+    registry.addResourceHandler('/**')
+        .addResourceLocations('classpath:/static')
+        .setCachePeriod(0)
+  }
+}
